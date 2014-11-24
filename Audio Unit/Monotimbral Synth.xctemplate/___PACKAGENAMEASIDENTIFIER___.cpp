140e438aba57cca47036fe40c5fd4557ed84416e
kbob <kbob@jogger-egg.com>
2014-11-23 18:08:27 -0800

    Lots of cleanup.  Reasonably well formatted and correctly commented.

--- Audio Unit/Monotimbral Synth.xctemplate/___PACKAGENAMEASIDENTIFIER___.cpp
+++ Audio Unit/Monotimbral Synth.xctemplate/___PACKAGENAMEASIDENTIFIER___.cpp
@@ -6,239 +6,315 @@
 //___COPYRIGHT___
 //
 
-/*
-	This is a test implementation of a sin wave synth using AUInstrumentBase classes
-	
-	It illustrates a basic usage of these classes
-	
-	It artificially limits the number of notes at one time to 12, so the note-stealing 
-	algorithm is used - you should know how this works!
-	
-	Most of the work you need to do is defining a Note class (see ___PACKAGENAMEASIDENTIFIER___Note). AUInstrument manages the
-	creation and destruction of notes, the various stages of a note's lifetime.
-
-	Alot of printfs have been left in (but are if'def out)
-	These can be useful as you figure out how this all fits together. This is true in the AUInstrumentBase
-	classes as well; simply define DEBUG_PRINT to 1 and this turns all this on
-	
-	The project also defines CA_AUTO_MIDI_MAP (OTHER_C_FLAGS). This adds all the code that is needed
-	to map MIDI messages to specific parameter changes. This can be seen in AU Lab's MIDI Editor window
-	CA_AUTO_MIDI_MAP is implemented in AUMIDIBase.cpp/.h
-*/
+// This is a test implementation of a sin wave synth using
+// AUInstrumentBase classes
+//
+//  It illustrates a basic usage of these classes
+//
+//  It artificially limits the number of notes at one time to 12, so
+//  the note-stealing algorithm is used - you should know how this
+//  works!
+//
+//  Most of the work you need to do is defining a Note class (see
+//  ___PACKAGENAMEASIDENTIFIER___Note). AUInstrument manages the
+//  creation and destruction of notes, the various stages of a note's
+//  lifetime.
+//
+//  Alot of printfs have been left in (but are if'def out) These can
+//  be useful as you figure out how this all fits together. This is
+//  true in the AUInstrumentBase classes as well; simply define
+//  DEBUG_PRINT to 1 and this turns all this on
+//
+//  The project also defines CA_AUTO_MIDI_MAP (OTHER_C_FLAGS). This
+//  adds all the code that is needed to map MIDI messages to specific
+//  parameter changes. This can be seen in AU Lab's MIDI Editor window
+//  CA_AUTO_MIDI_MAP is implemented in AUMIDIBase.cpp/.h
 
 #include "___FILEBASENAME___.h"
  
 static const UInt32 kMaxActiveNotes = 8;
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Define parameter name strings here.
+// static CFStringRef kParamName_One = CFSTR("First Parameter");
+// static CFStringRef kParamName_Two = CFSTR("Second Parameter");
 
-const double twopi = 2.0 * 3.14159265358979;
-
-inline double pow5(double x) { double x2 = x*x; return x2*x2*x; }
+////////////////////////////////////////////////////////////////////////////////
 
 #pragma mark ___PACKAGENAMEASIDENTIFIER___ Methods
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 AUDIOCOMPONENT_ENTRY(AUMusicDeviceFactory, ___PACKAGENAMEASIDENTIFIER___)
 
-static const AudioUnitParameterID kGlobalVolumeParam = 0;
-static const CFStringRef kGlobalVolumeName = CFSTR("global volume");
+// Define the presets.
+static AUPreset kPresets[kNumberOfPresets] = {
+    { kPreset_Default, CFSTR("Factory Default") },
+    // { kPreset_One, CFSTR("Preset One") },
+    // { kPreset_Two, CFSTR("Preset Two") },
+};
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//	___PACKAGENAMEASIDENTIFIER___::___PACKAGENAMEASIDENTIFIER___
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//      ___PACKAGENAMEASIDENTIFIER___::___PACKAGENAMEASIDENTIFIER___
 //
 // This synth has No inputs, One output
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ___PACKAGENAMEASIDENTIFIER___::___PACKAGENAMEASIDENTIFIER___(AudioUnit inComponentInstance)
-	: AUMonotimbralInstrumentBase(inComponentInstance, 0, 1)
+: AUMonotimbralInstrumentBase(inComponentInstance, 0, 1)
 {
-	CreateElements();
-	
-	Globals()->UseIndexedParameters(1); // we're only defining one param
-	Globals()->SetParameter (kGlobalVolumeParam, 1.0);
+    CreateElements();
+        
+    Globals()->UseIndexedParameters(kNumberOfParameters);
+    // Globals()->SetParameter(kParameter_One, 1.0);
+    // Globals()->SetParameter(kParameter_Two, 1.0);
+
+    SetAFactoryPresetAsCurrent(kPresets[kPreset_Default]);
 }
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//	___PACKAGENAMEASIDENTIFIER___::~___PACKAGENAMEASIDENTIFIER___
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//      ___PACKAGENAMEASIDENTIFIER___::~___PACKAGENAMEASIDENTIFIER___
 //
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ___PACKAGENAMEASIDENTIFIER___::~___PACKAGENAMEASIDENTIFIER___()
-{
-}
-
+{}
 
 void ___PACKAGENAMEASIDENTIFIER___::Cleanup()
 {
 #if DEBUG_PRINT
-	printf("___PACKAGENAMEASIDENTIFIER___::Cleanup\n");
+    printf("___PACKAGENAMEASIDENTIFIER___::Cleanup\n");
 #endif
 }
 
 OSStatus ___PACKAGENAMEASIDENTIFIER___::Initialize()
-{	
+{       
 #if DEBUG_PRINT
-	printf("->___PACKAGENAMEASIDENTIFIER___::Initialize\n");
+    printf("->___PACKAGENAMEASIDENTIFIER___::Initialize\n");
 #endif
-	AUMonotimbralInstrumentBase::Initialize();
-	
-	SetNotes(kNumNotes, kMaxActiveNotes, mNotes, sizeof(___PACKAGENAMEASIDENTIFIER___Note));
+    AUMonotimbralInstrumentBase::Initialize();
+        
+    SetNotes(kNumNotes, kMaxActiveNotes, mNotes, sizeof(___PACKAGENAMEASIDENTIFIER___Note));
 #if DEBUG_PRINT
-	printf("<-___PACKAGENAMEASIDENTIFIER___::Initialize\n");
+    printf("<-___PACKAGENAMEASIDENTIFIER___::Initialize\n");
 #endif
-	
-	return noErr;
+
+    return noErr;
 }
 
-AUElement* ___PACKAGENAMEASIDENTIFIER___::CreateElement(	AudioUnitScope					scope,
-									AudioUnitElement				element)
+AUElement* ___PACKAGENAMEASIDENTIFIER___::CreateElement(AudioUnitScope   scope,
+                                                        AudioUnitElement element)
 {
-	switch (scope)
-	{
-		case kAudioUnitScope_Group :
-			return new SynthGroupElement(this, element, new MidiControls);
-		case kAudioUnitScope_Part :
-			return new SynthPartElement(this, element);
-		default :
-			return AUBase::CreateElement(scope, element);
-	}
+    switch (scope) {
+
+    case kAudioUnitScope_Group:
+        return new SynthGroupElement(this, element, new MidiControls);
+
+    case kAudioUnitScope_Part:
+        return new SynthPartElement(this, element);
+
+    default:
+        return AUBase::CreateElement(scope, element);
+    }
 }
 
-OSStatus			___PACKAGENAMEASIDENTIFIER___::GetParameterInfo(		AudioUnitScope					inScope,
-														AudioUnitParameterID			inParameterID,
-														AudioUnitParameterInfo &		outParameterInfo)
+OSStatus ___PACKAGENAMEASIDENTIFIER___::GetParameterInfo(AudioUnitScope           inScope,
+                                    AudioUnitParameterID     inParameterID,
+                                    AudioUnitParameterInfo & outParameterInfo)
 {
-	if (inParameterID != kGlobalVolumeParam) return kAudioUnitErr_InvalidParameter;
-	if (inScope != kAudioUnitScope_Global) return kAudioUnitErr_InvalidScope;
+    AudioUnitParameterInfo& info = outParameterInfo;
+    info.flags = (kAudioUnitParameterFlag_IsWritable |
+                  kAudioUnitParameterFlag_IsReadable);
 
-	outParameterInfo.flags = SetAudioUnitParameterDisplayType (0, kAudioUnitParameterFlag_DisplaySquareRoot);
-    outParameterInfo.flags += kAudioUnitParameterFlag_IsWritable;
-	outParameterInfo.flags += kAudioUnitParameterFlag_IsReadable;
+    if (inScope != kAudioUnitScope_Global)
+        return kAudioUnitErr_InvalidScope;
 
-	AUBase::FillInParameterName (outParameterInfo, kGlobalVolumeName, false);
-	outParameterInfo.unit = kAudioUnitParameterUnit_LinearGain;
-	outParameterInfo.minValue = 0;
-	outParameterInfo.maxValue = 1.0;
-	outParameterInfo.defaultValue = 1.0;
-	return noErr;
+    switch (inParameterID) {
+
+    // case kParam_One:
+    //     info.flags = kAudioUnitParameterFlag_DisplaySquareRoot;
+    //     info.flags |= kAudioUnitParameterFlag_IsWritable;
+    //     info.flags |= kAudioUnitParameterFlag_IsReadable;
+
+    //     AUBase::FillInParameterName (info, kGlobalVolumeName, false);
+    //     info.unit = kAudioUnitParameterUnit_LinearGain;
+    //     info.minValue = 0;
+    //     info.maxValue = 1.0;
+    //     info.defaultValue = 1.0;
+    //     break;
+
+    // case kParam_Two:
+    //     info.flags = kAudioUnitParameterFlag_DisplaySquareRoot;
+    //     info.flags |= kAudioUnitParameterFlag_IsWritable;
+    //     info.flags |= kAudioUnitParameterFlag_IsReadable;
+
+    //     AUBase::FillInParameterName (info, kGlobalVolumeName, false);
+    //     info.unit = kAudioUnitParameterUnit_LinearGain;
+    //     info.minValue = 0;
+    //     info.maxValue = 1.0;
+    //     info.defaultValue = 1.0;
+    //     break;
+
+    default:
+        return kAudioUnitErr_InvalidParameter;
+    }
+
+    return noErr;
 }
 
 
 #pragma mark ___PACKAGENAMEASIDENTIFIER___Note Methods
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-void			___PACKAGENAMEASIDENTIFIER___Note::Release(UInt32 inFrame)
-{
-	SynthNote::Release(inFrame);
+bool ___PACKAGENAMEASIDENTIFIER___Note::Attack(const MusicDeviceNoteParams &inParams)
+{ 
 #if DEBUG_PRINT
-	printf("___PACKAGENAMEASIDENTIFIER___Note::Release %p %d\n", this, GetState());
+    printf("___PACKAGENAMEASIDENTIFIER___Note::Attack %p %d\n",
+           this, GetState());
+#endif
+    double sampleRate = SampleRate();
+    phase = 0.;
+    amp = 0.;
+    maxamp = 0.4 * pow(inParams.mVelocity/127., 3.); 
+    up_slope = maxamp / (0.1 * sampleRate);
+    dn_slope = -maxamp / (0.9 * sampleRate);
+    fast_dn_slope = -maxamp / (0.005 * sampleRate);
+    return true;
+}
+
+void ___PACKAGENAMEASIDENTIFIER___Note::Release(UInt32 inFrame)
+{
+    SynthNote::Release(inFrame);
+#if DEBUG_PRINT
+    printf("___PACKAGENAMEASIDENTIFIER___Note::Release %p %d\n",
+           this, GetState());
 #endif
 }
 
-void			___PACKAGENAMEASIDENTIFIER___Note::FastRelease(UInt32 inFrame) // voice is being stolen.
+void ___PACKAGENAMEASIDENTIFIER___Note::FastRelease(UInt32 inFrame) // voice is being stolen.
 {
-	SynthNote::Release(inFrame);
+    SynthNote::Release(inFrame);
 #if DEBUG_PRINT
-	printf("___PACKAGENAMEASIDENTIFIER___Note::Release %p %d\n", this, GetState());
+    printf("___PACKAGENAMEASIDENTIFIER___Note::Release %p %d\n",
+           this, GetState());
 #endif
 }
 
-void			___PACKAGENAMEASIDENTIFIER___Note::Kill(UInt32 inFrame) // voice is being stolen.
+Float32 ___PACKAGENAMEASIDENTIFIER___Note::Amplitude()
 {
-	SynthNote::Kill(inFrame);
+    return amp;
+}
+
+void ___PACKAGENAMEASIDENTIFIER___Note::Kill(UInt32 inFrame) // voice is being stolen.
+{
+    SynthNote::Kill(inFrame);
 #if DEBUG_PRINT
-	printf("___PACKAGENAMEASIDENTIFIER___Note::Kill %p %d\n", this, GetState());
+    printf("___PACKAGENAMEASIDENTIFIER___Note::Kill %p %d\n",
+           this, GetState());
 #endif
 }
 
-OSStatus		___PACKAGENAMEASIDENTIFIER___Note::Render(UInt64 inAbsoluteSampleFrame, UInt32 inNumFrames, AudioBufferList** inBufferList, UInt32 inOutBusCount)
+OSStatus ___PACKAGENAMEASIDENTIFIER___Note::Render(UInt64           inAbsoluteSampleFrame,
+                                                UInt32            inNumFrames,
+                                                AudioBufferList **inBufferList,
+                                                UInt32            inOutBusCount)
 {
-	float *left, *right;
-/* ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~	
-	Changes to this parameter (kGlobalVolumeParam) are not being de-zippered; 
-	Left as an exercise for the reader
- ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ */
-	float globalVol = GetGlobalParameter(kGlobalVolumeParam);
-	
-	// ___PACKAGENAMEASIDENTIFIER___Note only writes into the first bus regardless of what is handed to us.
-	const int bus0 = 0;
-	int numChans = inBufferList[bus0]->mNumberBuffers;
-	if (numChans > 2) return -1;
-	
-	left = (float*)inBufferList[bus0]->mBuffers[0].mData;
-	right = numChans == 2 ? (float*)inBufferList[bus0]->mBuffers[1].mData : 0;
+    float *left, *right;
+        
+    // ___PACKAGENAMEASIDENTIFIER___Note only writes into the first bus regardless of what is handed to us.
+    const int bus0 = 0;
+    int numChans = inBufferList[bus0]->mNumberBuffers;
+    if (numChans > 2)
+        return -1;
+        
+    left = (float*)inBufferList[bus0]->mBuffers[0].mData;
+    right = numChans == 2 ? (float*)inBufferList[bus0]->mBuffers[1].mData : 0;
 
-	double sampleRate = SampleRate();
-	double freq = Frequency() * (twopi/sampleRate);
+    double sampleRate = SampleRate();
+    double freq = Frequency() * (2 * M_PI / sampleRate);
 
-	
+        
 #if DEBUG_PRINT_RENDER
-	printf("___PACKAGENAMEASIDENTIFIER___Note::Render %p %d %g %g\n", this, GetState(), phase, amp);
+    printf("___PACKAGENAMEASIDENTIFIER___Note::Render %p %d %g %g\n",
+           this, GetState(), phase, amp);
 #endif
-	switch (GetState())
-	{
-		case kNoteState_Attacked :
-		case kNoteState_Sostenutoed :
-		case kNoteState_ReleasedButSostenutoed :
-		case kNoteState_ReleasedButSustained :
-			{
-				for (UInt32 frame=0; frame<inNumFrames; ++frame)
-				{
-					if (amp < maxamp) amp += up_slope;
-					float out = pow5(sin(phase)) * amp * globalVol;
-					phase += freq;
-					if (phase > twopi) phase -= twopi;
-					left[frame] += out;
-					if (right) right[frame] += out;
-				}
-			}
-			break;
-			
-		case kNoteState_Released :
-			{
-				UInt32 endFrame = 0xFFFFFFFF;
-				for (UInt32 frame=0; frame<inNumFrames; ++frame)
-				{
-					if (amp > 0.0) amp += dn_slope;
-					else if (endFrame == 0xFFFFFFFF) endFrame = frame;
-					float out = pow5(sin(phase)) * amp * globalVol;
-					phase += freq;
-					left[frame] += out;
-					if (right) right[frame] += out;
-				}
-				if (endFrame != 0xFFFFFFFF) {
-#if DEBUG_PRINT
-					printf("___PACKAGENAMEASIDENTIFIER___Note::NoteEnded  %p %d %g %g\n", this, GetState(), phase, amp);
-#endif
-					NoteEnded(endFrame);
-				}
-			}
-			break;
-			
-		case kNoteState_FastReleased :
-			{
-				UInt32 endFrame = 0xFFFFFFFF;
-				for (UInt32 frame=0; frame<inNumFrames; ++frame)
-				{
-					if (amp > 0.0) amp += fast_dn_slope;
-					else if (endFrame == 0xFFFFFFFF) endFrame = frame;
-					float out = pow5(sin(phase)) * amp * globalVol;
-					phase += freq;
-					left[frame] += out;
-					if (right) right[frame] += out;
-				}
-				if (endFrame != 0xFFFFFFFF) {
-#if DEBUG_PRINT
-					printf("___PACKAGENAMEASIDENTIFIER___Note::NoteEnded  %p %d %g %g\n", this, GetState(), phase, amp);
-#endif
-					NoteEnded(endFrame);
-				}
-			}
-			break;
-		default :
-			break;
-	}
-	return noErr;
-}
+    switch (GetState()) {
 
+    case kNoteState_Attacked :
+    case kNoteState_Sostenutoed :
+    case kNoteState_ReleasedButSostenutoed :
+    case kNoteState_ReleasedButSustained :
+        for (UInt32 frame=0; frame<inNumFrames; frame++) {
+            if (amp < maxamp)
+                amp += up_slope;
+            float sp = sinf(phase);
+            float sp2 = sp * sp;
+            float sp5 = sp2 * sp2 * sp;
+            float out = sp5 * amp;
+            phase += freq;
+            if (phase > 2 * M_PI)
+                phase -= 2 * M_PI;
+            left[frame] += out;
+            if (right)
+                right[frame] += out;
+        }
+        break;
+                        
+    case kNoteState_Released :
+        {
+            UInt32 endFrame = 0xFFFFFFFF;
+            for (UInt32 frame=0; frame<inNumFrames; frame++) {
+                if (amp > 0.0)
+                    amp += dn_slope;
+                else if (endFrame == 0xFFFFFFFF)
+                    endFrame = frame;
+                float sp = sinf(phase);
+                float sp2 = sp * sp;
+                float sp5 = sp2 * sp2 * sp;
+                float out = sp5 * amp;
+                phase += freq;
+                left[frame] += out;
+                if (right)
+                    right[frame] += out;
+            }
+            if (endFrame != 0xFFFFFFFF) {
+#if DEBUG_PRINT
+                printf("___PACKAGENAMEASIDENTIFIER___Note::NoteEnded  %p %d %g %g\n",
+                       this, GetState(), phase, amp);
+#endif
+                NoteEnded(endFrame);
+            }
+        }
+        break;
+                        
+    case kNoteState_FastReleased :
+        {
+            UInt32 endFrame = 0xFFFFFFFF;
+            for (UInt32 frame=0; frame<inNumFrames; ++frame) {
+                if (amp > 0.0)
+                    amp += fast_dn_slope;
+                else if (endFrame == 0xFFFFFFFF)
+                    endFrame = frame;
+                float sp = sinf(phase);
+                float sp2 = sp * sp;
+                float sp5 = sp2 * sp2 * sp;
+                float out = sp5 * amp;
+                phase += freq;
+                left[frame] += out;
+                if (right)
+                    right[frame] += out;
+            }
+            if (endFrame != 0xFFFFFFFF) {
+#if DEBUG_PRINT
+                printf("___PACKAGENAMEASIDENTIFIER___Note::NoteEnded  %p %d %g %g\n",
+                       this, GetState(), phase, amp);
+#endif
+                NoteEnded(endFrame);
+            }
+        }
+        break;
+
+    default :
+        break;
+    }
+
+    return noErr;
+}
--- Audio Unit/Monotimbral Synth.xctemplate/___PACKAGENAMEASIDENTIFIER___.h
+++ Audio Unit/Monotimbral Synth.xctemplate/___PACKAGENAMEASIDENTIFIER___.h
@@ -14,60 +14,62 @@
 
 static const UInt32 kNumNotes = 12;
 
-struct ___PACKAGENAMEASIDENTIFIER___Note : public SynthNote
-{
-	virtual					~___PACKAGENAMEASIDENTIFIER___Note() {}
+// Define constants to identify the parameters.
+enum Parameter {
+    // kParameter_Zero = 0,
+    // kParameter_One  = 1,
+    kNumberOfParameters
+};
 
-	virtual bool			Attack(const MusicDeviceNoteParams &inParams)
-								{ 
-#if DEBUG_PRINT
-									printf("___PACKAGENAMEASIDENTIFIER___Note::Attack %p %d\n", this, GetState());
-#endif
-									double sampleRate = SampleRate();
-									phase = 0.;
-									amp = 0.;
-									maxamp = 0.4 * pow(inParams.mVelocity/127., 3.); 
-									up_slope = maxamp / (0.1 * sampleRate);
-									dn_slope = -maxamp / (0.9 * sampleRate);
-									fast_dn_slope = -maxamp / (0.005 * sampleRate);
-									return true;
-								}
-	virtual void			Kill(UInt32 inFrame); // voice is being stolen.
-	virtual void			Release(UInt32 inFrame);
-	virtual void			FastRelease(UInt32 inFrame);
-	virtual Float32			Amplitude() { return amp; } // used for finding quietest note for voice stealing.
-	virtual OSStatus		Render(UInt64 inAbsoluteSampleFrame, UInt32 inNumFrames, AudioBufferList** inBufferList, UInt32 inOutBusCount);
+// Define constants to identify factory presets.
+enum Preset {
+    kPreset_Default = 0,
+    // kPreset_One  = 1,
+    // kPreset_Two  = 2,
+    kNumberOfPresets
+};
 
-	double phase, amp, maxamp;
-	double up_slope, dn_slope, fast_dn_slope;
+struct ___PACKAGENAMEASIDENTIFIER___Note : public SynthNote {
+
+ public:
+    virtual         ~___PACKAGENAMEASIDENTIFIER___Note() {}
+
+    virtual bool     Attack(const MusicDeviceNoteParams &inParams);
+    virtual void     Kill(UInt32 inFrame); // voice is being stolen.
+    virtual void     Release(UInt32 inFrame);
+    virtual void     FastRelease(UInt32 inFrame);
+    virtual Float32  Amplitude();
+    virtual OSStatus Render(UInt64            inAbsoluteSampleFrame,
+                            UInt32            inNumFrames,
+                            AudioBufferList** inBufferList,
+                            UInt32            inOutBusCount);
+
+ private:
+        double phase, amp, maxamp;
+        double up_slope, dn_slope, fast_dn_slope;
+
 };
 
 class ___PACKAGENAMEASIDENTIFIER___ : public AUMonotimbralInstrumentBase
 {
 public:
-								___PACKAGENAMEASIDENTIFIER___(AudioUnit inComponentInstance);
-	virtual						~___PACKAGENAMEASIDENTIFIER___();
-				
-	virtual OSStatus			Initialize();
-	virtual void				Cleanup();
-	virtual OSStatus			Version() { return k___PACKAGENAMEASIDENTIFIER___Version; }
+                       ___PACKAGENAMEASIDENTIFIER___(AudioUnit inComponentInstance);
+    virtual           ~___PACKAGENAMEASIDENTIFIER___();
+                                
+    virtual OSStatus   Initialize();
+    virtual void       Cleanup();
+    virtual OSStatus   Version() { return k___PACKAGENAMEASIDENTIFIER___Version; }
 
-	virtual AUElement*			CreateElement(			AudioUnitScope					scope,
-											  AudioUnitElement				element);
+    virtual AUElement *CreateElement(AudioUnitScope   scope,
+                                     AudioUnitElement element);
 
-	virtual OSStatus			GetParameterInfo(		AudioUnitScope					inScope,
-														AudioUnitParameterID			inParameterID,
-														AudioUnitParameterInfo &		outParameterInfo);
+    virtual OSStatus   GetParameterInfo(AudioUnitScope          inScope,
+                                    AudioUnitParameterID    inParameterID,
+                                    AudioUnitParameterInfo &outParameterInfo);
 
-	MidiControls*				GetControls( MusicDeviceGroupID inChannel)
-	{
-		SynthGroupElement *group = GetElForGroupID(inChannel);
-		return (MidiControls *) group->GetMIDIControlHandler();
-	}
-	
 private:
-	
-	___PACKAGENAMEASIDENTIFIER___Note mNotes[kNumNotes];
+    ___PACKAGENAMEASIDENTIFIER___Note mNotes[kNumNotes];
+
 };
 
 #endif /* !_____FILEBASENAME_____ */
